0d65466ce7b5945812539b690d92f6cb9ad1b3c1
TreeTse <1401078834@qq.com>
2019-07-16 22:02:02 +0800

    题目：二叉树的深度

---
+++ tree/ci_02.cpp
@@ -0,0 +1,23 @@
+/*
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};*/
+class Solution {
+public:
+    int TreeDepth(TreeNode* pRoot)
+    {
+        TreeNode* pSrc = pRoot;
+        if(pSrc == NULL){
+            if(count >= deplong)
+                return 
+        }
+    }
+private:
+    int count = 0;
+    int deplong = 0;
+};

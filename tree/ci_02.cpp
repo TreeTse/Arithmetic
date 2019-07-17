7220fbbc3e6c0313d984e437be84cd496428155d
TreeTse <1401078834@qq.com>
2019-07-17 09:22:13 +0800

    题目：二叉树的深度

--- tree/ci_02.cpp
+++ tree/ci_02.cpp
@@ -1,23 +1,30 @@
 /*
+题目描述：输入一棵二叉树，求该树的深度。从根结点到叶结点依次经过的结点
+（含根、叶结点）形成树的一条路径，最长路径的长度为树的深度。
+*/
+
+
+//解法：整棵二叉树的深度为根结点的节点1加上max（左子树的深度，右子树的深度）
+
+
+/*
 struct TreeNode {
-	int val;
-	struct TreeNode *left;
-	struct TreeNode *right;
-	TreeNode(int x) :
-			val(x), left(NULL), right(NULL) {
-	}
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+    TreeNode(int x) :
+            val(x), left(NULL), right(NULL) {
+    }
 };*/
 class Solution {
 public:
     int TreeDepth(TreeNode* pRoot)
     {
-        TreeNode* pSrc = pRoot;
-        if(pSrc == NULL){
-            if(count >= deplong)
-                return 
-        }
+        if(pRoot == nullptr)
+            return 0;
+        int nLeft = TreeDepth(pRoot->left);
+        int nRight = TreeDepth(pRoot->right);
+
+        return (nLeft > nRight)? (nLeft + 1):(nRight +1);
     }
-private:
-    int count = 0;
-    int deplong = 0;
 };

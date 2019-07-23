5487722658eef4ed171923bd2a0c94b9bf77e7ad
TreeTse <1401078834@qq.com>
2019-07-23 22:47:30 +0800

    题目：替换空格

--- string/ci_02.cpp
+++ string/ci_02.cpp
@@ -3,39 +3,39 @@
 We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
 */
 
+/*
+解法：先统计出空格的个数，并计算出替换之后的字符串的长度。然后从后往前去替换
+设置两个指针p1和p2，一个指向原始字符串末尾，一个指向替换后的字符串末尾，移动p1和p2，逐个把p1指向的
+字符复制到p2指针的位置
+*/
 
 class Solution {
 public:
 	void replaceSpace(char *str,int length) {
-        if(str == NULL && length <= 0){
+        if(str == NULL && length <= 0)
             return;
-        }
-               /*original_length为字符串str的实际长度*/
-        int original_length = 0;            //原始长度
-        int number_blank = 0;				//空格数
+        int new_length = 0;   //新的字符串长度
+        int count = 0;    //计算空格数
         int i;
-        while(str[i++] != '\0'){				//遍历字符串
-            ++original_length;				//长度+1
-            if(str[i] == ' '){
-                ++number_blank;				//遇到空格+1
-            }
+        char *p_orign = NULL;    //用来定位到原始字符串末尾的指针
+        char *p_new = NULL;       //用来定位到新字符串末尾的指针
+        while(str[i++] != '\0'){
+            if(str[i] == ' ')
+                count++;
         }
-        /*new_length为把空格替换成'%20'之后的长度*/
-        int new_length = original_length + 2 * number_blank;
-        int index_original = original_length;	//原始字符串末尾索引值
-        int index_new = new_length;				//计算长度后的字符串末尾索引值
-        
-        /*index_original指针开始向前移动，如果遇到空格，替换成'%20'，否则进行复制操作*/
-        while(index_original >= 0 && index_new > index_original){
-            if(str[index_original] == ' '){
-                str[index_new--] = '0';
-                str[index_new--] = '2';
-                str[index_new--] = '%';
+        new_length = length + 2*count;    
+        p_orign = str + length -1;
+        p_new = str + new_length -1;
+        while(length >= 0 && p_orign != p_new){
+            if(*p_orign == ' '){
+                *(p_new--) = '0';
+                *(p_new--) = '2';
+                *(p_new--) = '%';
             }
             else{
-                str[index_new--] = str[index_original];
+                *(p_new--) = *p_orign;
             }
-            --index_original;
+            p_orign--;
         }
 	}
 };

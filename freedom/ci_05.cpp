833edda01c081b33fa3665b5e1a6ea92a60d037d
TreeTse <1401078834@qq.com>
2019-08-30 20:18:49 +0800

    题目：顺时针打印矩阵

--- freedom/ci_05.cpp
+++ freedom/ci_05.cpp
@@ -1,28 +1,34 @@
+/*
+题目描述：
+输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字，例如，如果输入如下4 X 4矩阵： 
+1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 则依次打印出数字1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10.
+*/
 
+
+//设左上角和右下角两个点，顺时针打印一圈后，左上角的点向右下角跳一格，右下角的点向左上角跳一格。
 
 
 class Solution {
 public:
     vector<int> printMatrix(vector<vector<int> > matrix) {
-        vector<int>res;
-        res.clear();
-        int row=matrix.size();//行数
-        int collor=matrix[0].size();//列数
-        //计算打印的圈数
-        int circle=((row<collor?row:collor)-1)/2+1;//圈数
-        for(int i=0;i<circle;i++){
-            //从左向右打印
-            for(int j=i;j<collor-i;j++)
-                res.push_back(matrix[i][j]);         
-            //从上往下的每一列数据
-            for(int k=i+1;k<row-i;k++)
-                res.push_back(matrix[k][collor-1-i]);
-            //判断是否会重复打印(从右向左的每行数据)
-            for(int m=collor-i-2;(m>=i)&&(row-i-1!=i);m--)
-                res.push_back(matrix[row-i-1][m]);
-            //判断是否会重复打印(从下往上的每一列数据)
-            for(int n=row-i-2;(n>i)&&(collor-i-1!=i);n--)
-                res.push_back(matrix[n][i]);}
-        return res;
+        vector<int> getMatrix;
+        int i;
+        int left = 0, right = matrix[0].size()-1, top = 0, bottom = matrix.size()-1;
+        if(right+1 == 0 || bottom+1 == 0)
+            return getMatrix;
+        while(left <= right && top <= bottom){
+            for(i = left; i <= right; ++i)
+                getMatrix.push_back(matrix[top][i]);
+            for(i = top+1; i <= bottom; ++i)
+                getMatrix.push_back(matrix[i][right]);
+            if(top != bottom)
+            for(i = right-1; i >= left; --i)
+                getMatrix.push_back(matrix[bottom][i]);
+            if(left != right)
+            for(i = bottom-1; i > top; --i)
+                getMatrix.push_back(matrix[i][left]);
+            left++; top++; right--; bottom--;
+        }
+        return getMatrix;
     }
 };

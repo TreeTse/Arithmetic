6ec84014676ea5057fbc234c31e366dd8401f9dd
TreeTse <1401078834@qq.com>
2019-10-27 21:31:47 +0800

    题目：把字符串转换成整数

--- string/ci_05.cpp
+++ string/ci_05.cpp
@@ -24,3 +24,22 @@
 
 */
 
+
+
+class Solution {
+public:
+    int StrToInt(string str) {
+        int n = str.size(), s = 1;
+        long long res = 0;
+        if(!n) return 0;
+        if(str[0] == '-') s = -1;
+        for(int i = (str[0] ==  '-' || str[0] == '+') ? 1 : 0; i < n; ++i){
+            if(!('0' <= str[i] && str[i] <= '9')) return 0;
+            res = (res << 1) + (res << 3) + (str[i] & 0xf);//res=res*10+str[i]-'0';
+        } 
+        if(res*s >= -2147483648 && res*s <= 2147483647)
+            return res*s;
+        else 
+            return 0;
+    }
+};

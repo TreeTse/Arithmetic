3b1d2abfac603598f339b53d6818a588f848d989
TreeTse <1401078834@qq.com>
2019-12-10 00:04:12 +0800

    题目:数字在排序数组中出现的次数

--- reserch/ci_02.cpp
+++ reserch/ci_02.cpp
@@ -5,7 +5,7 @@
 
 
 
-//解法：直接使用C++ STL库
+//解法1：直接使用C++ STL库
 
 class Solution {
 public:
@@ -17,22 +17,22 @@ public:
 };
 
 
-
+//解法2：因为data中都是整数，所以可以稍微变一下，不是搜索k的两个位置，而是搜索k-0.5和k+0.5
+//这两个数应该插入的位置，然后相减即可。
 class Solution {
 public:
     int GetNumberOfK(vector<int> data ,int k) {
-        return biSearch(data, k+0.5) - biSearch(data, k-0.5) ;
+        return BiSearch(data,k+0.5) - BiSearch(data,k-0.5);
     }
-private:
-    int biSearch(const vector<int> & data, double num){
-        int s = 0, e = data.size()-1;     
-        while(s <= e){
-            int mid = (e - s)/2 + s;
-            if(data[mid] < num)
-                s = mid + 1;
-            else if(data[mid] > num)
-                e = mid - 1;
-        }
-        return s;
-    }
+    int BiSearch(const vector<int>& array,double num){
+        int l = 0 , r = array.size()-1;
+        while(l <= r){
+            int mid = (r + l)/2;
+            if(array[mid] < num)
+                l = mid + 1;
+            else if(array[mid] > num)
+                r = mid - 1;
+         }
+         return l;
+     }
 };

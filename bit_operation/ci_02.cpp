9fe27cdcac5189518cc80757c577cdd51b42f50b
TreeTse <1401078834@qq.com>
2019-12-12 11:09:07 +0800

    题目：不用加减乘除做加法

--- bit_operation/ci_02.cpp
+++ bit_operation/ci_02.cpp
@@ -4,11 +4,22 @@
 */
 
 
+/*
+解法：
+1、两个数异或运算，相当于每一位相加，而不考虑进位
+2、两个数相与运算，并左移一位，相当于求得进位
+3、将上述两步结果相加
+*/
+
 class Solution {
 public:
     int Add(int num1, int num2){
-        int a = num1 ^ num2;
-        int b = (num1 & num2) << 1;
-        return a + b;
+        while(num2 != 0){
+            int a = num1 ^ num2;
+            int b = (num1 & num2) << 1;
+            num1 = a;
+            num2 = b;
+        }
+        return num1;
     }
 };
